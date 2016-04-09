3fc7dd16606e604583d1e10a0e8fd9a696136ea5
Eduardo Russo <russoedu@gmail.com>
2016-04-09 08:27:29 -0300

    Removed unused file

--- server/fields.js
+++
@@ -1,9 +0,0 @@
-"field_name":{
-    "type":"number",
-    // button, checkbox, color, date, datetime, datetime-local,
-    // email, file, hidden, image, month, number, password, radio, range,
-    // reset, search, submit, tel, text, time, week
-    "max-length":4,
-    "min-length":4,
-    "required":true
-},
