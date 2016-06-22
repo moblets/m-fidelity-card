c9e38d9f50b0f96ffa53c89551fd2817197d333a
Eduardo Russo <russoedu@gmail.com>
2016-06-22 17:17:41 -0300

    [master] Wirking again

--- moblet/m-fidelity-card.js
+++ moblet/m-fidelity-card.js
@@ -19,42 +19,60 @@ module.exports = {
     $ionicPopup,
     $uAlert
   ) {
-      // While there is no local data, show the loader
-    // $scope.$watch("data", function() {
-    //   $timeout(function() {
-    //     if (typeof $scope.data !== "undefined") {
-    //       if (typeof $localStorage["uFidelityCard::quantity"] === "undefined") {
-    //         $scope.quantity = $scope.data.quantity;
-    //         $scope.password = $scope.data.password;
-    //         $loading.hide();
-    //       }
-    //       $localStorage["uFidelityCard::password"] = $scope.data.password;
-    //       $localStorage["uFidelityCard::quantity"] = parseInt(
-    //         $scope.data.quantity, 10
-    //       );
-    //     }
-    //   }, 10);
-    // });
+    var lsKey = 'mFidelityCard' + $scope.moblet.id + '::';
+
+    var testPassword = function(password) {
+      var cryptoJS = require('crypto-js');
+      /* eslint new-cap: 0 */
+      return cryptoJS.SHA1($scope.salt + password).toString();
+    };
+
+    var setStamps = function() {
+      $scope.stamped = $localStorage[lsKey + 'stamped'] || 0;
+
+      $scope.stamps = new Array($scope.quantity);
+
+      for (var i = 0; i < $scope.stamped; i++) {
+        $scope.stamps[i] = 1;
+      }
+    };
+
+    var confirmFullCard = function() {
+      $ionicPopup.confirm({
+        title: $filter('translate')("reward_confirm_title"),
+        template: $filter('translate')("reward_confirm"),
+        buttons: [{
+          text: $filter('translate')("cancel")
+        }, {
+          text: '<b>' + $filter('translate')("yes_uppercase") + '</b>',
+          onTap: function() {
+            $scope.stamped = 0;
+            $localStorage[lsKey + 'stamped'] = 0;
+            setStamps();
+            $uAlert.shows(
+              $filter('translate')("reward_title"),
+              $filter('translate')("reward_cicle_reset"),
+              'OK'
+            );
+          }
+        }]
+      });
+    };
 
     var init = function() {
       $scope.isLoading = true;
       $scope.form = {password: ""};
 
-      var cryptoJS = require('crypto-js');
       var dataLoadOptions = {cache: false};
 
-      $scope.testPassword = function(password) {
-        /* eslint new-cap: 0 */
-        cryptoJS.SHA1($scope.card.salt + password).toString();
-      };
-
+      // Load data from Moblets
       $mDataLoader.load($scope.moblet, dataLoadOptions)
         .then(function(card) {
           $scope.password = card.password;
           $scope.description = card.description;
-          $scope.quantity = card.quantity;
+          $scope.quantity = Number(card.quantity);
           $scope.salt = card.salt;
-          console.log(card.stampEmpty);
+
           $scope.stampEmpty = card.stampEmpty || 'https://s3.amazonaws.com/' +
           'static.fabricadeaplicativos.com.br/moblets/' +
           'u-fidelity-card/stamp-off.png';
@@ -62,86 +80,59 @@ module.exports = {
           'static.fabricadeaplicativos.com.br/moblets/' +
           'u-fidelity-card/stamp-on.png';
 
-          $scope.stamped = $localStorage[
-            'mFidelityCard' +
-            $scope.moblet.id +
-            '::stamped'] || 0;
-
-          $scope.stamps = new Array(parseInt($scope.quantity, 10));
-
-          for (var i = 0; i < $scope.stamped; i++) {
-            $scope.stamps[i] = 1;
-          }
-          $scope.stamps[0] = 1;
+          // Create the stamps array
+          setStamps();
 
           $scope.isLoading = false;
         }
       );
-      // $scope.quantity = $localStorage["uFidelityCard::quantity"];
-      // $scope.stamped = $localStorage["uFidelityCard::stamped"] || 0;
-      // $scope.password = $localStorage["uFidelityCard::password"];
-
-    //   if (
-    //     typeof $scope.quantity === "undefined" &&
-    //   typeof $scope.password === "undefined"
-    // ) {
-    //     $loading.show();
-    //   }
     };
 
-    // $scope.stamp = function() {
-    //   var title;
-    //   var buttonText;
-    //   if ($scope.stamped === $scope.quantity) {
-    //     title = $filter('translate')("enter_reward_password");
-    //     buttonText = $filter('translate')("reward_button");
-    //   } else {
-    //     title = $filter('translate')("enter_stamp_password");
-    //     buttonText = $filter('translate')("stamp_button");
-    //   }
-    //   $ionicPopup.show({
-    //     template: '<input autofocus type="password" ng-model="form.password">',
-    //     title: title,
-    //     scope: $scope,
-    //     buttons: [{
-    //       text: $filter('translate')("cancel")
-    //     }, {
-    //       text: '<b>' + buttonText + '</b>',
-    //       onTap: function(e) {
-    //         if ($scope.password === $scope.form.password) {
-    //           $scope.form.password = "";
-    //           if ($scope.stamped < $scope.quantity) {
-    //                 // don't allow the user to close unless he enters stamp password
-    //             $scope.stamped += 1;
-    //             $localStorage["uFidelityCard::stamped"] = $scope.stamped;
-    //           } else {
-    //             $scope.confirmFullCard();
-    //           }
-    //         } else {
-    //           $uAlert.toast($filter('translate')("wrong_pass"));
-    //           e.preventDefault();
-    //         }
-    //       }
-    //     }]
-    //   });
-    // };
-    //
-    //
-    // $scope.confirmFullCard = function() {
-    //   $ionicPopup.confirm({
-    //     title: $filter('translate')("reward_confirm"),
-    //     buttons: [{
-    //       text: $filter('translate')("cancel")
-    //     }, {
-    //       text: '<b>' + $filter('translate')("yes_uppercase") + '</b>',
-    //       onTap: function() {
-    //         $scope.stamped = 0;
-    //         $localStorage["uFidelityCard::stamped"] = 0;
-    //         $uAlert.toast($filter('translate')("stamp_card_reward"));
-    //       }
-    //     }]
-    //   });
-    // };
+    $scope.stamp = function() {
+      var title;
+      var buttonText;
+
+      if ($scope.stamped === $scope.quantity) {
+        title = $filter('translate')("enter_reward_password");
+        buttonText = $filter('translate')("reward_button");
+      } else {
+        title = $filter('translate')("enter_stamp_password");
+        buttonText = $filter('translate')("stamp_button");
+      }
+
+      $ionicPopup.show({
+        template: '<input autofocus id="password" ' +
+          'type="number" ng-model="form.password">',
+        title: title,
+        scope: $scope,
+        buttons: [{
+          text: $filter('translate')("cancel")
+        }, {
+          text: '<b>' + buttonText + '</b>',
+          onTap: function(e) {
+            if ($scope.password === testPassword($scope.form.password)) {
+              $scope.form.password = "";
+              if ($scope.stamped === $scope.quantity) {
+                confirmFullCard();
+              } else {
+                $timeout(function() {
+                  $scope.stamps[$scope.stamped] = 1;
+                  $scope.stamped += 1;
+                  $localStorage[lsKey + 'stamped'] = $scope.stamped;
+                }, 10);
+              }
+            } else {
+              $uAlert.shows(
+                $filter('translate')("wrong_pass"),
+                $filter('translate')("try_again"),
+                $filter('translate')("go_back")
+              );
+              e.preventDefault();
+            }
+          }
+        }]
+      });
+    };
 
     init();
   }
